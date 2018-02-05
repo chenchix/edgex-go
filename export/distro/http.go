76e711073e9bc6cf0d669a47c75946ea5b194d4d
chencho <smunoz@caviumnetworks.com>
2018-02-05 13:42:00 +0100

    Modified url creation. Now use procotol from addressable
    Signed-off-by: chencho <smunoz@caviumnetworks.com>

--- export/distro/http.go
+++ export/distro/http.go
@@ -29,7 +29,7 @@ func NewHTTPSender(addr export.Addressable) Sender {
 	// CHN: Should be added protocol from Addressable instead of include it the address param.
 	// CHN: We will maintain this behaviour for compatibility with Java
 	sender := httpSender{
-		url:    addr.Address + ":" + strconv.Itoa(addr.Port) + addr.Path,
+		url:    addr.Protocol + "://" + addr.Address + ":" + strconv.Itoa(addr.Port) + addr.Path,
 		method: addr.Method,
 	}
 	return sender
